47d32117f3dfee0037e1ffab332b884dd93fd678
tsubasa1122 <nakanot@open8.com>
2019-07-20 22:44:09 +0900

    seedsファイルの作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_20_131636) do
+ActiveRecord::Schema.define(version: 2019_07_20_132153) do
 
   create_table "members", force: :cascade do |t|
     t.integer "number", null: false
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 2019_07_20_131636) do
     t.boolean "administrator", default: false, null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "phone"
   end
 
 end
