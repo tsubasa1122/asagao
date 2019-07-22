725ec15b92565553ba13ddf5a2cfa2f7b3481cf7
tsubasa1122 <nakanot@open8.com>
2019-07-22 22:45:55 +0900

    記事一覧作成

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,7 @@
+class ArticlesController < ApplicationController
+  before_action :login_required, except: [:index, :show]
+
+  def index
+    @articles = Article.order(released_at: :desc)
+  end
+end
