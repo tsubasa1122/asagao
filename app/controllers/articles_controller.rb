87a179623ebc24d6697bfbdff27292652a845407
tsubasa1122 <nakanot@open8.com>
2019-07-23 09:58:25 +0900

    記事作成機能実装

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,5 +3,54 @@ class ArticlesController < ApplicationController
 
   def index
     @articles = Article.order(released_at: :desc)
+
+    @articles = @articles.open_to_the_public unless current_member
+
+    unless current_member&.administrator?
+      @atricles = @articles.visible
+    end
+  end
+
+  def show
+    articles = Article.all
+    articles = articles.open_to_the_public unless current_member
+
+    unless current_member&.administrator?
+      articles = visible
+    end
+    @article = articles.find(params[:id])
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(params[:article])
+    if @article.save
+      redirect_to @article, notice: "ニュース記事を登録しました"
+    else
+      render "new"
+    end
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    @article.assign_attributes(params[:article])
+    if @article.save
+      redirect_to @article, notice: "ニュース記事を更新しました"
+    else
+      render "edit"
+    end
+  end
+
+  def destroy
+    @article = @article.find(params[:id])
+    @article.destroy
+    redirect_to :articles
   end
 end
