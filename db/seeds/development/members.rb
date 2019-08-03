bf97340f51824366f4ceff7823ce18fe18a117dd
tsubasa1122 <nakanot@open8.com>
2019-08-03 15:52:53 +0900

    画像投稿seedデータ投入

--- db/seeds/development/members.rb
+++ db/seeds/development/members.rb
@@ -29,3 +29,11 @@ end
     password_confirmation: "asagao!"
   )
 end
+
+filename = "profile.png"
+path = Rails.root.join(__dir__, filename)
+m = Member.find_by!(number: 10)
+
+File.open(path) do |f|
+  m.profile_picture.attach(io: f, filename: filename)
+end
