61510e21f004ef18a08978ebc8b1680099cef984
tsubasa1122 <nakanot@open8.com>
2019-08-11 18:31:14 +0900

    memberの操作をadmin側に移す

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
     get "lesson/step#{n}/(:name)" => "lesson#step#{n}"
   end
 
-  resources :members do
+  resources :members, only: [:index, :show] do
     get "search", on: :collection
     resources :entries, only: [:index]
   end
@@ -27,5 +27,8 @@ Rails.application.routes.draw do
 
   namespace :admin do
     root "top#index"
+    resources :members do
+      get "search", on: :collection
+    end
   end
 end
